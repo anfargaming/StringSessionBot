4f9fad2354e86bdf16694364f0c5e38c21828173
anfargaming <120932993+anfargaming@users.noreply.github.com>
2023-01-31 01:23:24 +0530

    Update __init__.py

--- StringSessionBot/database/__init__.py
+++ StringSessionBot/database/__init__.py
@@ -1,7 +1,20 @@
 from env import DATABASE_URL
-from sqlalchemy import create_engine
+from sqlalchemy import Column, BigInteger, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+BASE = declarative_base()
+
+
+class Users(BASE):
+    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
+    user_id = Column(BigInteger, primary_key=True)
+
+    def __init__(self, user_id, channels=None):
+        self.user_id = user_id
+        self.channels = channels
 
 
 def start() -> scoped_session:
@@ -11,5 +24,5 @@ def start() -> scoped_session:
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
-BASE = declarative_base()
 SESSION = start()
+
